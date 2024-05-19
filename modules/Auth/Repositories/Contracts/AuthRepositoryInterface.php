352fe7778740b6a39ffa9cd7ebe4f95bc36554fc
nhuandt26 <dothenhuan26@gmail.com>
2024-05-19 16:54:05 +0700

    Add: modules Auth

---
+++ modules/Auth/Repositories/Contracts/AuthRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Auth\Repositories\Contracts;
+
+use Modules\Core\Repositories\Contracts\RepositoryInterface;
+
+interface AuthRepositoryInterface extends RepositoryInterface
+{
+
+}
